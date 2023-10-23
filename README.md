70bec71a2275392bad302a784b3058619a42356a
zcienka <zuzannacienka@gmail.com>
2023-10-23 21:46:25 +0200

    Initialize Electron and React

---
+++ README.md
@@ -0,0 +1,17 @@
+# Metadata Editor
+The Metadata Editor is a desktop application that allows users to edit the metadata of their files. 
+It is intended to be used by musicians to edit the metadata of their music files. 
+
+## Technology stack
+The application is built using the following technologies:
+- [Electron](https://www.electronjs.org/)
+- [React](https://reactjs.org/)
+- [TypeScript](https://www.typescriptlang.org/)
+- [Tailwind CSS](https://tailwindcss.com/)
+- [Flowbite](https://flowbite.com/)
+
+## Running the application
+To run the application run the following command:
+```
+yarn electron:serve
+```
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}",
+        "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+        "./pages/**/*.{html,js}",
+        "./components/**/*.{html,js}",
+    ],
+    theme: {
+        fontFamily: {
+            "roboto": ["Roboto", "sans-serif"],
+        },
+        extend: {
+            spacing: {
+                "128": "32rem",
+                "160": "40rem",
+                "192": "48rem",
+                "224": "52rem",
+                "256": "64rem",
+                "320": "72rem",
+            },
+            colors: {},
+        }
+    },
+    plugins: ["tailwindcss, autoprefixer", "flowbite/plugin"],
+}
