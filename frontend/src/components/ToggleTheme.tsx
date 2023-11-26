8e330b7fb2173c1dbef530372d61bdd847183f1b
zcienka <zuzannacienka@gmail.com>
2023-11-26 22:27:36 +0100

    Fix bug with theme toggling

--- frontend/src/components/ToggleTheme.tsx
+++ frontend/src/components/ToggleTheme.tsx
@@ -3,9 +3,11 @@ import { ReactComponent as SunIcon } from "../assets/icons/sun.svg"
 import { ReactComponent as MoonIcon } from "../assets/icons/moon.svg"
 
 const ToggleTheme = () => {
-    const [theme, setTheme] = useState("dark")
+    const [theme, setTheme] = useState(localStorage.getItem("theme") || "dark")
 
     useEffect(() => {
+        localStorage.setItem("theme", theme)
+
         if (theme === "light") {
             document.documentElement.classList.remove("dark")
         } else {
@@ -14,21 +16,19 @@ const ToggleTheme = () => {
     }, [theme])
 
     const toggleTheme = () => {
-        if (theme === "light") {
-            setTheme("dark")
-        } else {
-            setTheme("light")
-        }
+        setTheme(prevTheme => prevTheme === "light" ? "dark" : "light")
     }
 
-    return <button type="button"
-                   className="text-gray-800 bg-white border border-gray-300 focus:outline-none
-                              hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm
-                               p-2.5 mr-2 dark:bg-gray-800 dark:text-white dark:border-gray-600
-                               dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
-                   onClick={toggleTheme}>
-        {theme === "light" ? <MoonIcon className="w-5 h-5" /> : <SunIcon className="w-5 h-5" />}
-    </button>
+    return (
+        <button type="button"
+                className="text-gray-800 bg-white border border-gray-300 focus:outline-none
+                           hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm
+                           p-2.5 mr-2 dark:bg-gray-800 dark:text-white dark:border-gray-600
+                           dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                onClick={toggleTheme}>
+            {theme === "light" ? <MoonIcon className="w-5 h-5" /> : <SunIcon className="w-5 h-5" />}
+        </button>
+    )
 }
 
 export default ToggleTheme
