5d8abc7ef1ac2359bd4c31ae0923c2272a833386
zcienka <zuzannacienka@gmail.com>
2023-12-06 00:10:39 +0100

    Add batch deletion and checkbox functionality

--- backend/routes/collection.ts
+++ backend/routes/collection.ts
@@ -5,11 +5,13 @@ const router = express.Router()
 const {
     getAllCollections,
     getCollection,
-    createCollection
+    createCollection,
+    batchDeleteCollections
 } = require("../controllers/collections")
 
 router.route("/").get(getAllCollections)
 router.route("/").post(createCollection)
 router.route("/:id").get(getCollection)
+router.route("/:collection").delete(batchDeleteCollections)
 
 module.exports = router
