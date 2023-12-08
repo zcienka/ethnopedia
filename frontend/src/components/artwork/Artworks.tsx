6c207f5d0d26cc2319cfde83bf50494c5f382c5a
zcienka <zuzannacienka@gmail.com>
2023-12-08 18:46:30 +0100

    Add alphabetical sorting for collections and artworks

--- frontend/src/components/artwork/Artworks.tsx
+++ frontend/src/components/artwork/Artworks.tsx
@@ -11,6 +11,7 @@ import { ReactComponent as FileImportIcon } from "../../assets/icons/fileImport.
 import { ReactComponent as FileExportIcon } from "../../assets/icons/fileExport.svg"
 import CreateArtwork from "./CreateArtwork"
 import WarningPopup from "../../pages/collections/WarningPopup"
+import CustomDropdown from "../CustomDropdown"
 
 const Artworks = () => {
     const location = useLocation()
@@ -18,7 +19,7 @@ const Artworks = () => {
     const [showFileDropzone, setShowFileDropzone] = useState<boolean>(false)
     const [showCreateArtwork, setShowCreateArtwork] = useState<boolean>(false)
     const [showWarningPopup, setShowWarningPopup] = useState(false)
-
+    const [sortOrder, setSortOrder] = useState<string>("default")
     const queryClient = useQueryClient()
 
     const { mutate: batchDeleteMutation } = useBatchDeleteArtworkMutation()
@@ -27,6 +28,13 @@ const Artworks = () => {
         const params = new URLSearchParams(location.search)
         return params.toString()
     }, [location.search])
+
+    const sortOptions = [
+        { value: "title-asc", label: "Tytuł rosnąco" },
+        { value: "title-desc", label: "Tytuł malejąco" },
+        { value: "year-asc", label: "Rok rosnąco" },
+        { value: "year-desc", label: "Rok malejąco" },
+    ]
 
     const { data: fetchedData } = useQuery({
         queryKey: ["artwork", searchParamsString],
@@ -49,6 +57,25 @@ const Artworks = () => {
         // XLSX.utils.book_append_sheet(wb, ws, "DataSheet")
         // XLSX.writeFile(wb, "DataExport.xlsx")
     }
+
+    const sortArtworks = (artworks: any[], order: string) => {
+        switch (order) {
+            case "title-asc":
+                return artworks.sort((a, b) => a.Tytuł.localeCompare(b.Tytuł))
+            case "title-desc":
+                return artworks.sort((a, b) => b.Tytuł.localeCompare(a.Tytuł))
+            case "year-asc":
+                return artworks.sort((a, b) => a.Rok - b.Rok)
+            case "year-desc":
+                return artworks.sort((a, b) => b.Rok - a.Rok)
+            default:
+                return artworks
+        }
+    }
+
+    const sortedArtworks = useMemo(() => {
+        return sortArtworks([...(fetchedData || [])], sortOrder)
+    }, [fetchedData, sortOrder])
 
     const handleCheck = (id: string) => {
         setSelectedArtworks((prev) => ({ ...prev, [id]: !prev[id] }))
@@ -77,7 +104,7 @@ const Artworks = () => {
     if (fetchedData === undefined) {
         return <LoadingPage />
     } else {
-        const allArtworks = fetchedData.map((artwork: any) => (
+        const allArtworks = sortedArtworks.map((artwork: any) => (
             <div className="px-4 py-4 bg-white dark:bg-gray-800 shadow-md rounded-lg mb-4 border border-gray-300 dark:border-gray-600
             cursor-pointer"
                  key={artwork._id}
@@ -113,82 +140,87 @@ const Artworks = () => {
             <section className="bg-gray-50 dark:bg-gray-900 p-3 sm:p-5 h-full">
                 <div className="mx-auto max-w-screen-xl lg:px-12">
                     <SearchComponent />
-                    <div className="flex items-center space-x-3 w-full md:w-auto mb-4">
-                        <button
-                            type="button"
-                            className="flex items-center justify-center dark:text-white bg-white
+                    <div className="flex  w-full md:w-auto mb-4">
+                        <div className="flex flex-1 space-x-3">
+                            <button
+                                type="button"
+                                className="flex items-center justify-center dark:text-white bg-white
                                     hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium
                                     text-sm px-4 py-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none
                                     dark:focus:ring-primary-800"
-                            onClick={() => setShowCreateArtwork(showCreateArtwork => !showCreateArtwork)}>
+                                onClick={() => setShowCreateArtwork(showCreateArtwork => !showCreateArtwork)}>
 
                         <span className="mr-2 text-gray-500 dark:text-gray-400">
                             <PlusIcon />
                         </span>
-                            Nowy rekord
-                        </button>
-                        <button
-                            className="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm
+                                Nowy rekord
+                            </button>
+                            <button
+                                className="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm
                                         font-medium text-gray-900 focus:outline-none bg-white rounded-lg border
                                         hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4
                                         dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-                            type="button"
-                            onClick={() => exportToExcel()}
-                        >
+                                type="button"
+                                onClick={() => exportToExcel()}
+                            >
                         <span className="text-gray-400 dark:text-gray-400">
                             <FileExportIcon />
                         </span>
-                            Eksportuj plik
-                        </button>
-                        <button
-                            className="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm
+                                Eksportuj plik
+                            </button>
+                            <button
+                                className="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm
                                         font-medium text-gray-900 focus:outline-none bg-white rounded-lg border
-                                        hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4
                                         dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-                            type="button"
-                            onClick={() => setShowFileDropzone(showFileDropzone => !showFileDropzone)}
-                        >
+                                type="button"
+                                onClick={() => setShowFileDropzone(showFileDropzone => !showFileDropzone)}
+                            >
                         <span className="text-gray-400 dark:text-gray-400">
                             <FileImportIcon />
                         </span>
-                            Importuj plik
-                        </button>
+                                Importuj plik
+                            </button>
 
-                        <button
-                            className="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm
+                            <button
+                                className="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm
                                         font-medium text-gray-900 focus:outline-none bg-white rounded-lg border
-                                        hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4
                                         dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-                            type="button"
-                            onClick={selectAll}
-                        >
-                            Zaznacz wszystkie
-                        </button>
+                                type="button"
+                                onClick={selectAll}
+                            >
+                                Zaznacz wszystkie
+                            </button>
 
-                        <button
-                            className="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm
+                            <button
+                                className="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm
                                         font-medium text-gray-900 focus:outline-none bg-white rounded-lg border
-                                        hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4
                                         dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-                            type="button"
-                            onClick={deselectAll}
-                        >
-                            Odznacz wszystkie
-                        </button>
+                                type="button"
+                                onClick={deselectAll}
+                            >
+                                Odznacz wszystkie
+                            </button>
 
-                        <button
-                            className="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm
+                            <button
+                                className="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm
                                         font-medium text-gray-900 focus:outline-none bg-white rounded-lg border
-                                        hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4
                                         dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-                            type="button"
-                            onClick={() => {
-                                if (Object.keys(selectedArtworks).length !== 0)
-                                    setShowWarningPopup(!showWarningPopup)
-                            }}
-                        >
-                            Usuń zaznaczone
-                        </button>
+                                type="button"
+                                onClick={() => {
+                                    if (Object.keys(selectedArtworks).length !== 0)
+                                        setShowWarningPopup(!showWarningPopup)
+                                }}
+                            >
+                                Usuń zaznaczone
+                            </button>
+                        </div>
+
+                        <span className="">
+                            <CustomDropdown
+                                options={sortOptions}
+                                onSelect={(value) => setSortOrder(value)}
+                            />
+                        </span>
                     </div>
                     {showFileDropzone && <FileDropzone onClose={() => setShowFileDropzone(false)} />}
                     {allArtworks}
