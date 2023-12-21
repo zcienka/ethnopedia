47f32c09b52b5f5329bd3d2d597596c0bdc65b49
WiktoriaSzymanska <wiktoria.szymanska@student.put.poznan.pl>
2023-12-21 21:11:51 +0100

    different input form

--- frontend/src/components/artwork/NewArtworkStructure.tsx
+++ frontend/src/components/artwork/NewArtworkStructure.tsx
@@ -1,367 +1,106 @@
-import React, { useState } from "react"
-import { ReactComponent as PlusIcon } from "../../assets/icons/plus.svg"
-import { ReactComponent as MinusIcon } from "../../assets/icons/minus.svg"
+import React, { useState, ChangeEvent, FormEvent } from 'react';
 
-interface Category {
-  category: string;
-  values?: string[];
-  subcategories?: Subcategory[];
+interface Metadata {
+  [key: string]: string | Subcategory;
 }
 
-interface Subcategory {
-  name: string;
-  values: string[];
+interface Subcategory extends Array<string | Metadata> {
+  0: string;
+  1: Metadata;
 }
-
-interface JSONTree {
-  _id: string;
-  collection: string;
-  categories: Category[];
-}
-
-
-const jsonData: JSONTree[] = [
-    {
-        "_id": "6562758966826fe944b0fc2f",
-        "collection": "Wielkopolska",
-        "categories": [
-            {
-                "category": "Sygnatura nagrania",
-                "subcategories": [
-                    {
-                        "name": "Pozycja",
-                        "values": ["Rec001", "Rec002", "Rec003"],
-                    },
-                ],
-            },
-            {
-                "category": "Incipit",
-                "subcategories": [
-                    {
-                        "name": "Język incipitu",
-                        "values": [
-                            "angielski",
-                            "niemiecki",
-                            "polski",
-                        ],
-                    },
-                ],
-            },
-            {
-                "category": "Nazwisko wykonawcy",
-                "subcategories": [
-                    {
-                        "name": "Wykonawca",
-                        "values": ["Kowalski", "Nowak", "Wiśniewska"],
-                    },
-                ],
-            },
-            {
-                "category": "Numer wątku muzycznego",
-                "values": ["Muz001", "Muz002", "Muz003"],
-            },
-            {
-                "category": "Numer w publikacji",
-                "values": ["Pub001", "Pub002", "Pub003"],
-            },
-            {
-                "category": "Sposób wykonania",
-                "subcategories": [
-                    {
-                        "name": "Barwa głosu",
-                        "values": ["baryton", "sopran", "tenor"],
-                    },
-                    {
-                        "name": "Tempo wykonania",
-                        "values": ["szybko", "umiarkowanie", "wolno"],
-                    },
-                    {
-                        "name": "Ornamentyka",
-                        "values": ["tryle", "mordenty", "vibrato"],
-                    },
-                ],
-            },
-            {
-                "category": "Funkcja utworu ogólnie",
-                "subcategories": [
-                    {
-                        "name": "Szczegółowa funkcja",
-                        "values": ["taniec", "kołysanka", "pieśń ludowa"],
-                    },
-                    {
-                        "name": "Funkcja określona przez wykonawcę",
-                        "values": ["obrzędowa", "rozrywkowa", "ceremonialna"],
-                    },
-                ],
-            },
-            {
-                "category": "Numer wątku melodycznego",
-                "values": ["Mel001", "Mel002", "Mel003"],
-            },
-            {
-                "category": "Wykorzystanie w publikacji",
-                "values": ["Publikacja A", "Publikacja B", "Publikacja C"],
-            },
-            {
-                "category": "Klasyfikacja melodyczna",
-                "subcategories": [
-                    {
-                        "name": "Rytmika",
-                        "values": ["4/4", "3/4", "6/8"],
-                    },
-                    {
-                        "name": "Metrum",
-                        "values": ["wolne", "umiarkowane", "szybkie"],
-                    },
-                    {
-                        "name": "Forma melodyczna",
-                        "values": ["ABA", "ABC", "AABB"],
-                    },
-                    {
-                        "name": "Skala",
-                        "values": ["majorowa", "minorowa", "chromatyczna"],
-                    },
-                    {
-                        "name": "Ambitus",
-                        "values": ["1 oktawa", "1.5 oktawy", "2 oktawy"],
-                    },
-                    {
-                        "name": "Układ kadencji",
-                        "values": ["prosta", "rozbudowana", "modulująca"],
-                    },
-                ],
-            },
-            {
-                "category": "Struktura tekstu",
-                "subcategories": [
-                    {
-                        "name": "Ilość wersów",
-                        "values": ["4", "8", "12"],
-                    },
-                    {
-                        "name": "Układ sylab w wersie",
-                        "values": ["7-6-7-6", "8-8-8-8", "5-5-5-5"],
-                    },
-                ],
-            },
-            {
-                "category": "Uwagi",
-                "subcategories": [
-                    {
-                        "name": "Stan techniczny nagrania",
-                        "values": ["doskonały", "dobry", "zadowalający"],
-                    },
-                    {
-                        "name": "Walory melodii",
-                        "values": ["chwytliwa", "melancholijna", "rytmiczna"],
-                    },
-                ],
-            },
-            {
-                "category": "Obecność w źródłach",
-                "subcategories": [
-                    {
-                        "name": "Antologie",
-                        "values": ["Antologia A", "Antologia B", "Antologia C"],
-                    },
-                    {
-                        "name": "Rękopisy",
-                        "values": ["Rękopis 1", "Rękopis 2", "Rękopis 3"],
-                    },
-                    {
-                        "name": "Śpiewniki",
-                        "values": ["Śpiewnik 1", "Śpiewnik 2", "Śpiewnik 3"],
-                    },
-                ],
-            },
-        ],
-    },
-]
 
 const NewArtworkStructure: React.FC = () => {
-    const [selectedCategories, setSelectedCategories] = useState<string[]>(jsonData.map((item, index) => `cat-${index}-${item.collection}`))
+  const [formDataList, setFormDataList] = useState<Metadata[]>([{ key: '', value: '' }]);
+  const [jsonData, setJsonData] = useState<string>('');
 
-    const handleCategoryChange = (
-        event: React.ChangeEvent<HTMLSelectElement>,
-        itemIndex: number,
-        catIndex: number,
-    ) => {
-        const updatedCategories = [...selectedCategories]
-        updatedCategories[catIndex] = `cat-${itemIndex}-${event.target.value}`
-        setSelectedCategories(updatedCategories)
-    }
+  const handleInputChange = (index: number, e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormDataList((prevDataList) =>
+      prevDataList.map((item, i) => (i === index ? { ...item, [name]: value } : item))
+    );
+  };
 
-    const removeCategory = (catIndex: number) => {
-        const updatedCategories = selectedCategories.filter((_, index) => index !== catIndex)
-        setSelectedCategories(updatedCategories)
-    }
+  const handleAddCategory = () => {
+    setFormDataList((prevDataList) => [...prevDataList, { key: '', value: '' }]);
+  };
 
-    const addCategory = () => {
-        setSelectedCategories([...selectedCategories, ""])
-    }
+  const handleAddSubcategory = (index: number) => {
+    setFormDataList((prevDataList) =>
+      prevDataList.map((item, i) =>
+        i === index
+          ? {
+              ...item,
+              value: Array.isArray(item.value)
+                ? [...item.value, { key: '', value: '' }]
+                : ['', { key: '', value: '' }],
+            } as unknown as Metadata
+          : item
+      )
+    );
+  };
 
-    return <div className="p-4 max-w-screen-md">
-        {jsonData.map((item, index) => (
-            <div key={`${item._id}-${index}`}>
-                <h2 className="font-bold">{item.collection}</h2>
+  const handleGenerateJson = () => {
+    const newData: Metadata = {};
+    formDataList.forEach((item) => {
+      if (typeof item.key == 'string' && item.value) {
+        newData[item.key] = item.value;
+      } else if (Array.isArray(item.key) && item.key.length > 0) {
+        newData[item.key[0]] = item.value;
+      }
+    });
+    setJsonData(JSON.stringify(newData, null, 2));
+  };
 
-                <div className="ml-2 border-l-2 border-gray-300">
-                    {selectedCategories.map((selectedCategory, catIndex) => (
-                        <CategoryAndValueSelector
-                            key={catIndex}
-                            item={item}
-                            selectedCategory={selectedCategory}
-                            handleCategoryChange={handleCategoryChange}
-                            handleCategoryDeletion={() => removeCategory(catIndex)}
-                            index={index}
-                            catIndex={catIndex}
-                        />
-                    ))}
-                </div>
-                
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log('Form submitted:', formDataList);
+  };
 
-                <div className="ml-2 flex flex-row relative">
-                    <span className="absolute bg-gray-300 h-1/2 w-0.5"></span>
+  const handleRemove = (index: number) => {
+    setFormDataList((prevDataList) => prevDataList.filter((_, i) => i !== index));
+  };
 
-                    <hr className="border-t-2 border-gray-300 dark:border-gray-700 w-8 self-center" />
-                    <button onClick={addCategory} className="p-2 border-gray-300 shadow-md" type="button">
-                        <PlusIcon />
-                    </button>
-                </div>
-            </div>
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        {formDataList.map((formData, index) => (
+          <div key={index}>
+            <label>
+              <input
+                type="text"
+                name={`key`}
+                value={typeof formData.key === 'string' ? formData.key : formData.key[0]}
+                onChange={(e) => handleInputChange(index, e)}
+              />
+            </label>
+            <label>
+              :
+              <input
+                type="text"
+                name={`value`}
+                value={typeof formData.value === 'string' ? formData.value : formData.value[0]}
+                onChange={(e) => handleInputChange(index, e)}
+              />
+            </label>
+            <button type="button" onClick={() => handleRemove(index)}>
+              -
+            </button>
+            <button type="button" onClick={() => handleAddSubcategory(index)}>
+              +
+            </button>
+          </div>
         ))}
+        <button type="button" onClick={handleAddCategory}>
+          Add Another Value
+        </button>
+        <button type="button" onClick={handleGenerateJson}>
+          Show JSON
+        </button>
+        <button type="submit">Submit</button>
+      </form>
+      <pre>{jsonData}</pre>
     </div>
-}
+  );
+};
 
-interface CategorySelectorProps {
-    item: JSONTree
-    selectedCategory: string
-    handleCategoryChange: (event: React.ChangeEvent<HTMLSelectElement>, itemIndex: number, catIndex: number) => void
-    handleCategoryDeletion: (catIndex: number) => void
-    index: number
-    catIndex: number
-}
-
-const CategoryAndValueSelector: React.FC<CategorySelectorProps> = ({
-                                                                       item,
-                                                                       selectedCategory,
-                                                                       handleCategoryChange,
-                                                                       handleCategoryDeletion,
-                                                                       index,
-                                                                       catIndex,
-                                                                   }) => {
-    const [categoryValue, setCategoryValue] = useState<string>('')
-    const [subcategoryValues, setSubcategoryValues] = useState<{ [key: string]: string }>({})
-
-    const selectedCategoryIndex = selectedCategory.startsWith(`cat-${index}-`) ? Number(selectedCategory.split("-")[2]) : null
-    const selectedCategoryData = selectedCategoryIndex !== null ? item.categories[selectedCategoryIndex] : null
-    
-    const [selectedSubcategories, setSelectedSubcategories] = useState<string[]>(selectedCategoryData && selectedCategoryData.subcategories ? selectedCategoryData.subcategories.map(subcat => subcat.name) : [])
-
-    const addSubcategorySelect = () => {
-        setSelectedSubcategories([...selectedSubcategories, ""])
-    }
-
-    const handleSubcategoryChange = (subcatIndex: number, newName: string) => {
-        const updatedSubcategories = [...selectedSubcategories]
-        updatedSubcategories[subcatIndex] = newName
-        setSelectedSubcategories(updatedSubcategories)
-    }
-
-    const removeSubcategorySelect = (subcatIndex: number) => {
-        const updatedSubcategories = selectedSubcategories.filter((_, index) => index !== subcatIndex)
-        setSelectedSubcategories(updatedSubcategories)
-    }
-
-    // Find the selected category
-    // const selectedCategoryIndex = selectedCategory.startsWith(`cat-${index}-`) ? Number(selectedCategory.split("-")[2]) : null;
-    // const selectedCategoryData = selectedCategoryIndex !== null ? item.categories[selectedCategoryIndex] : null;
-
-    return (
-        <div className="flex flex-col pb-2">
-            <div className="flex flex-row">
-
-                <hr className="border-t-2 border-gray-300 dark:border-gray-700 w-8 self-center" />
-                <div className="p-2 border shadow-md rounded-md mt-2">
-                    <select
-                        className="p-2 border rounded"
-                        onChange={(e) => handleCategoryChange(e, index, catIndex)}
-                        value={selectedCategory.startsWith(`cat-${index}-`) ? selectedCategory.split("-")[2] : ""}
-                    >
-                        <option value="">Select a Category</option>
-                        {item.categories.map((category, categoryIndex) => (
-                            <option key={`${category.category}-${categoryIndex}`} value={categoryIndex}>
-                                {category.category}
-                            </option>
-                        ))}
-                    </select>
-                    <input
-                        type="text"
-                        className="p-2 border rounded ml-2"
-                        placeholder="Enter category value"
-                        value={categoryValue}
-                        onChange={(e) => setCategoryValue(e.target.value)}
-                    />
-                </div>
-
-                <div className="flex items-center">
-                    <button onClick={addSubcategorySelect} className="mx-2 p-2 shadow-md" type="button">
-                        <PlusIcon />
-                    </button>
-                    <button onClick={() => handleCategoryDeletion(catIndex)} className="p-2 shadow-md" type="button">
-                        <MinusIcon />
-                    </button>
-                </div>
-            </div>
-
-
-            {selectedSubcategories.length !== 0 && selectedCategoryData && <div className="flex flex-col">
-                <div className="ml-8 flex flex-row relative">
-                    <div>
-                        {selectedSubcategories.map((subcatName, subcatIndex) => (
-                            <div className="flex flex-row items-center relative">
-
-                                {subcatIndex !== selectedSubcategories.length - 1 ?
-                                    <span className="absolute bg-gray-300 h-full w-0.5"></span>
-                                    : <span className="top-0 absolute bg-gray-300 h-1/2 w-0.5"></span>
-                                }
-                                <hr className="border-t-2 border-gray-300 dark:border-gray-700 w-16 self-center" />
-
-                                <div key={subcatIndex}
-                                    className="flex flex-row items-center p-2 border shadow-md rounded-md mt-2">
-                                    <select
-                                        className="p-2 border rounded"
-                                        value={subcatName}
-                                        onChange={(e) => handleSubcategoryChange(subcatIndex, e.target.value)}
-                                        >
-                                        <option value="">Select a Subcategory</option>
-                                        {selectedCategoryData.subcategories?.map((subcat, subcatIdx) => (
-                                            <option key={`subcat-${subcatIdx}`} value={subcat.name}>
-                                                {subcat.name}
-                                            </option>
-                                        ))}
-                                    </select>
-                                    <input
-                                        type="text"
-                                        className="p-2 border rounded ml-2"
-                                        placeholder="Enter subcategory value"
-                                        value={subcategoryValues[subcatIndex] || ''}
-                                        onChange={(e) => setSubcategoryValues({ ...subcategoryValues, [subcatIndex]: e.target.value })}
-                                    />
-                                </div>
-
-                                <button onClick={() => removeSubcategorySelect(subcatIndex)}
-                                        className="ml-2 border shadow-md p-2" type="button">
-                                    <MinusIcon />
-                                </button>
-                            </div>
-                        ))}
-
-                    </div>
-                </div>
-            </div>}
-        </div>
-    )
-}
-
-export default NewArtworkStructure
+export default NewArtworkStructure;
