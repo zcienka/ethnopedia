5f24d939ab6ebf4c3400b8c734da2b0f0f25c958
mateusz-napiorkowski <mateusz.napiorkowski0@gmail.com>
2024-03-17 14:05:05 +0100

    Add basic import of data with subcategories

--- backend/controllers/importData.ts
+++ backend/controllers/importData.ts
@@ -2,20 +2,72 @@ import { NextFunction, Request, Response } from "express"
 import { getMongoDBNativeDriverClient } from "../db/connect"
 const mongoClient = getMongoDBNativeDriverClient()
 
+interface subData {
+    name: string
+    values: string
+    subcategories: Array<any>,
+    isSelectable: boolean
+}
+
+const fillSubcategories: any = (depth: number, fields: any, allAttrs: any, header: any, recordsData: any, recordIndex: number) => {
+    let subs: any = []
+    let deeperFields: any = []
+    fields.forEach((field: any) => {
+        header.forEach((attrName: string, elementIndex: number) => {
+            if(attrName.startsWith(field) && attrName.split(".").length === depth + 1) {
+                deeperFields.push(attrName)
+            }
+        });
+        let newSub: subData = {name: field.split(".").slice(-1)[0],
+            values: recordsData[recordIndex][header.indexOf(field)].toString().split(";").filter((i: any) => i !== ""),
+            subcategories: [],
+            isSelectable: false
+        }
+        if(deeperFields.length !== 0) {
+            newSub.subcategories = fillSubcategories(depth + 1, deeperFields, allAttrs, header, recordsData, recordIndex)
+        }
+        subs.push(newSub)
+    });
+    return subs
+}
+
+
+
 const importData = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const header = req.body[0]
+        let headerAttrsInArrays: any = []
+        header.forEach((attr:string, index:String) => {
+            headerAttrsInArrays.push(attr.split("."))
+        });
+        const recordsData = req.body.slice(1)
         const records: Array<[]> = []
-        for (const key in req.body) {
-            if (req.body.hasOwnProperty(key) && key != "0") {
-                const recordData = req.body[key];
-                const record: any = {}
-                for (const recordDataKey in recordData) {
-                    const value = recordData[recordDataKey]
-                    record[header[recordDataKey]] = value
+        for(const recordIndex in recordsData) {
+            const recordAttrs = recordsData[recordIndex]
+            let newRecord: any = {categories: []}
+            let depth = 1
+            const categories: any = newRecord.categories
+            recordAttrs.forEach((cellVal: any, attrIndex: number) => {
+                if(header[attrIndex].split(".").length === depth) {
+                    let fields: any = []
+                    header.forEach((element: any, elementIndex: number) => {
+                        if(element.startsWith(header[attrIndex]) && element.split(".").length === depth + 1) {
+                            fields.push(element)
+                        }
+                    });
+                    const newCaterory: subData = {
+                        name: header[attrIndex], 
+                        values: recordAttrs[attrIndex].toString().split(";").filter((i: any) => i !== ""),
+                        subcategories: [],
+                        isSelectable: false
+                    }
+                    if(fields.length !== 0) {
+                        newCaterory.subcategories = fillSubcategories(depth + 1, fields, recordAttrs, header, recordsData, recordIndex)
+                    }
+                    categories.push(newCaterory)
                 }
-                records.push(record)
-            }
+            });
+            records.push(newRecord)
         }
         mongoClient.db().collection('artworks').insertMany(records)
     } catch (error) {
